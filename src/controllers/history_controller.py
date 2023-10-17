b50bed0d414a06f7db72933ab39235c7125ce6ab
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-10-17 19:57:57 -0300

    req 8 erro nao encontrou url

---
+++ src/controllers/history_controller.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+import json
+from models.history_model import HistoryModel
+
+
+history_controller = Blueprint("history_controller", __name__)
+
+
+@history_controller.route("/history", methods=["GET"])
+def index():
+    translation_history_str = HistoryModel.list_as_json()
+    translation_history_json = json.loads(translation_history_str)
+    return translation_history_json, 200
--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request
 from deep_translator import GoogleTranslator
 from models.language_model import LanguageModel
 
-# from models.history_model import HistoryModel
+from models.history_model import HistoryModel
 
 
 translate_controller = Blueprint("translate_controller", __name__)
@@ -12,26 +12,26 @@ translate_controller = Blueprint("translate_controller", __name__)
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
     text_to_translate = (
-        request.form.get("text-to-translate")
-        if request.method == "POST"
-        else "O que deseja traduzir"
+        request.form.get("text-to-translate") or "O que deseja traduzir"
     )
-    translate_from = (
-        request.form.get("translate-from")
-        if request.method == "POST"
-        else "pt"
-    )
-    translate_to = (
-        request.form.get("translate-to") if request.method == "POST" else "en"
-    )
-    translated = (
-        GoogleTranslator(source=translate_from, target=translate_to).translate(
-            text_to_translate
-        )
-        if request.method == "POST"
-        else "Tradução"
-    )
+    translate_from = request.form.get("translate-from", "pt")
+    translate_to = request.form.get("translate-to", "en")
     languages = LanguageModel.list_dicts()
+
+    if request.method == "POST":
+        translated = GoogleTranslator(
+            source=translate_from, target=translate_to
+        ).translate(text_to_translate)
+        translation_history = {
+            "text_to_translate": text_to_translate,
+            "translate_from": translate_from,
+            "translate_to": translate_to,
+        }
+        history = HistoryModel(translation_history)
+        history.save()
+    else:
+        translated = 'Tradução'
+        
     return render_template(
         "index.html",
         languages=languages,
