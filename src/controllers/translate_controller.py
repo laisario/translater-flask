aa23f9d70dc6fa1d10b841a8c92ba9686483857d
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-10-17 16:35:55 -0300

    problemas req 4

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+
 # from deep_translator import GoogleTranslator
-# from models.language_model import LanguageModel
+from models.language_model import LanguageModel
+
 # from models.history_model import HistoryModel
 
 
@@ -10,7 +12,20 @@ translate_controller = Blueprint("translate_controller", __name__)
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    text_to_translate = (
+        request.form.get("text-to-translate") or "O que deseja traduzir"
+    )
+    translate_from = request.form.get('translate-from') or 'pt'
+    translate_to = request.form.get('translate-to') or 'en'
+    translated = 'Tradução'
+    return render_template(
+        "index.html",
+        languages=LanguageModel.list_dicts(),
+        text_to_translate=text_to_translate,
+        translate_from=translate_from,
+        translate_to=translate_to,
+        translated=translated
+    )
 
 
 # Req. 6
