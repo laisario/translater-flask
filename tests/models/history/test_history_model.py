a98f72b630ce962389e379eb1943aab6c50e4630
Laisa Rio Verde <laisa.rioverde@gmail.com>
2023-10-17 19:07:27 -0300

    checando se req 7 passou msm

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,25 @@
 # import json
-# from src.models.history_model import HistoryModel
+from src.models.history_model import HistoryModel
+import json
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    expected = [
+        {
+            "text_to_translate": "Hello, I like videogame",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+        {
+            "text_to_translate": "Do you love music?",
+            "translate_from": "en",
+            "translate_to": "pt",
+        },
+    ]
+    json_list = HistoryModel.list_as_json()
+    json_converted = json.loads(json_list)
+    for dicionario in json_converted:
+        dicionario.pop("_id", None)
+    print(">>>>>>>>>>>", json_converted, "<<<<<<<<<<<<<<<")
+    assert json_converted == expected
